2048189e55f1691c9c67d7a19d3f17238cfe878f
wangwei_b <wangwei_b@xiaomi.com>
2012-04-21 10:47:15 +0800

    modify app patch script to support multi paths
    Change-Id: I7d4766f47e56d3bed79cc9be1db3212a31520a1e
    Signed-off-by: wangwei_b <wangwei_b@xiaomi.com>

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -5,6 +5,8 @@
 #
 
 XMLMERGYTOOL=$PORT_ROOT/tools/ResValuesModify/jar/ResValuesModify
+GIT_APPLY=$PORT_ROOT/tools/git.apply
+
 curdir=`pwd`
 
 if [ $1 = "Phone" ];then
@@ -27,13 +29,13 @@ if [ $1 = "ThemeManager" ];then
 fi
 
 if [ $1 = "Settings" ];then
-    cp $1/Settings.part out/
+    cp $1/*.part out/
     cd out
-    git.apply Settings.part
+    $GIT_APPLY Settings.part
     cd ..
     for file in `find $2 -name *.rej`
     do
-	echo "Settings patch fail"
+	echo "Fatal error: Settings patch fail"
         exit 1
     done
 
